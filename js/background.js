ef4fb22a703eaba8e533507a4fa5206d06bec9ab
milworm <ruslan.prytula@gmail.com>
2015-12-17 23:28:15 -0500

    screen video recording + audio using MediaRecorder API works

--- js/background.js
+++ js/background.js
@@ -1,145 +1,22 @@
 'use strict';
 
-var FsHelper = new function () {
-    var fs;
-
-    this.readFileAsDataUrl = function (fileName) {
-        return new Promise(function (resolve, error) {
-            if (fs) return;
-
-            window.webkitRequestFileSystem(window.PERSISTENT, 1024 * 1024 * 100, function (fileSystem) {
-                resolve(fs = fileSystem);
-            });
-        }).then(function () {
-            return new Promise(function (resolve, error) {
-                fs.root.getFile(fileName, {}, resolve);
-            });
-        }).then(function (fileEntry) {
-            return new Promise(function (resolve, error) {
-                fileEntry.file(resolve, error);
-            });
-        }).then(function (file) {
-            return new Promise(function (resolve, error) {
-                var reader = new FileReader();
-                reader.onloadend = function () {
-                    resolve(reader.result);
-                };
-                reader.readAsDataURL(file);
-            });
-        });
-    };
-}();
-
-function startRecording(callback) {
-    var screenId, getVideoStream, getAudioStream, videoEncoder, streams;
-
-    getVideoStream = function () {
-        return new Promise(function (resolve, error) {
-            navigator.webkitGetUserMedia({
-                audio: false,
-                video: {
-                    mandatory: {
-                        chromeMediaSource: 'desktop',
-                        chromeMediaSourceId: screenId,
-                        maxWidth: screen.width,
-                        maxHeight: screen.height
-                    }
-                }
-            }, resolve, error);
-        });
-    };
-
-    getAudioStream = function () {
-        return new Promise(function (resolve, error) {
-            navigator.webkitGetUserMedia({
-                audio: true
-            }, resolve, error);
-        });
-    };
-
-    return new Promise(function (resolve, reject) {
-        chrome.desktopCapture.chooseDesktopMedia(['screen', 'window'], resolve);
-    }).then(function (id) {
-        screenId = id;
-
-        return Promise.all([getVideoStream(), getAudioStream()]);
-    }).then(function (config) {
-        streams = {
-            video: config[0],
-            audio: config[1]
-        };
-    }).then(function () {
-        return new Promise(function (resolve, error) {
-            videoEncoder = document.createElement("embed");
-
-            videoEncoder.setAttribute("width", 0);
-            videoEncoder.setAttribute("height", 0);
-            videoEncoder.setAttribute("src", "/nacl/manifest.nmf");
-            videoEncoder.setAttribute("ps_tty_prefix", "ps:");
-            videoEncoder.setAttribute("ps_stdout", "/dev/tty");
-            videoEncoder.setAttribute("ps_stderr", "/dev/tty");
-            videoEncoder.setAttribute("type", "application/x-nacl");
-
-            videoEncoder.addEventListener("load", resolve);
-
-            videoEncoder.addEventListener("message", function () {
-                console.log(arguments);
-            });
-
-            videoEncoder.addEventListener("crash", function () {
-                console.log(arguments);
-            });
-
-            // Request the offsetTop property to force a relayout. As of Apr 10, 2014
-            // this is needed if the module is being loaded on a Chrome App's
-            // background page (see crbug.com/350445).
-            document.body.appendChild(videoEncoder);
-            videoEncoder.offsetTop;
-        });
-    }).then(function () {
-        videoEncoder.postMessage({
-            type: "start",
-            data: {
-                filename: "/html5_persistent/output.webm",
-                chromeVersion: 46,
-                videoTrack: streams.video.getVideoTracks()[0],
-                audioTrack: streams.audio.getAudioTracks()[0]
-            }
-        });
-
-        return new Promise(function (resolve, error) {
-            streams.video.onended = resolve;
-        });
-    }).then(function () {
-        videoEncoder.postMessage({
-            type: "stop",
-            data: {}
-        });
-    }).then(function () {
-        [streams.video, streams.audio].forEach(function (stream) {
-            stream.getTracks().forEach(function (track) {
-                track.stop();
-            });
-        });
-    }).then(function () {
-        return FsHelper.readFileAsDataUrl("output.webm");
-    }).then(callback);
-}
-
 function Recorder() {}
 Recorder.prototype = {
     start: function start(callback) {
-        this.stream = null;
         this.recorder = null;
         this.buffer = [];
 
-        this.getSource().then(this.getStream.bind(this)).then(this.record.bind(this)).then(this.cleanup.bind(this)).then(this.save.bind(this)).then(callback);
+        this.getSource().then(this.initStreams.bind(this)).then(this.mergeStreams.bind(this)).then(this.record.bind(this)).then(this.cleanup.bind(this)).then(this.save.bind(this)).then(callback);
     },
 
-    getStream: function getStream(sourceId) {
+    initStreams: function initStreams(sourceId) {
+        return Promise.all([this.getVideoStream(sourceId), this.getAudioStream()]);
+    },
+
+    getVideoStream: function getVideoStream(sourceId) {
         return new Promise(function (resolve, error) {
             navigator.webkitGetUserMedia({
-                audio: true,
+                audio: false,
                 video: {
                     mandatory: {
                         chromeMediaSource: 'desktop',
@@ -150,6 +27,23 @@ Recorder.prototype = {
                 }
             }, resolve, error);
         });
+    },
+
+    getAudioStream: function getAudioStream() {
+        return new Promise(function (resolve, error) {
+            navigator.webkitGetUserMedia({
+                audio: true,
+                video: false
+            }, resolve, error);
+        });
+    },
+
+    mergeStreams: function mergeStreams(streams) {
+        var video = streams[0],
+            audio = streams[1];
+
+        video.addTrack(audio.getTracks()[0]);
+        return video;
     },
 
     getSource: function getSource() {
@@ -167,7 +61,7 @@ Recorder.prototype = {
         return new Promise(function (resolve, error) {
             var recorder = new MediaRecorder(stream);
 
-            recorder.ondataavailable = function () {
+            recorder.ondataavailable = function (e) {
                 if (e.data) me.buffer.push(e.data);
 
                 clearTimeout(timerId);
@@ -184,7 +78,7 @@ Recorder.prototype = {
             recorder = this.recorder;
 
         return new Promise(function (resolve, reject) {
-            stream.getTracks().forEach(function (track) {
+            recorder.stream.getTracks().forEach(function (track) {
                 track.stop();
             });
 
@@ -197,11 +91,17 @@ Recorder.prototype = {
     },
 
     save: function save() {
-        var buffer = this.buffer;
+        var blob = new Blob(this.buffer, {
+            type: "video/webm"
+        });
 
-        return new Promise(function (resolve, reject) {
-            console.log(buffer);
-            resolve();
+        return new Promise(function (resolve, error) {
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                resolve(reader.result);
+            };
+
+            reader.readAsDataURL(blob);
         });
     }
 };
--- src/background.js
+++ src/background.js
@@ -1,164 +1,29 @@
-var FsHelper = new function() {
-    var fs;
-
-    this.readFileAsDataUrl = function(fileName) {
-        return new Promise(function(resolve, error) {
-            if(fs)
-                return ;
-
-            window.webkitRequestFileSystem(window.PERSISTENT, 1024 * 1024 * 100, function(fileSystem) {
-                resolve(fs = fileSystem);
-            });
-        }).then(function() {
-            return new Promise(function(resolve, error) {
-                fs.root.getFile(fileName, {}, resolve);
-            });
-        }).then(function(fileEntry) {
-            return new Promise(function(resolve, error) {
-                fileEntry.file(resolve, error);
-            });
-        }).then(function(file) {
-            return new Promise(function(resolve, error) {
-                var reader = new FileReader();
-                reader.onloadend = function() {
-                    resolve(reader.result);
-                };
-                reader.readAsDataURL(file);
-            });
-        })
-    }
-}
-
-function startRecording(callback) {
-    var screenId,
-        getVideoStream,
-        getAudioStream,
-        videoEncoder,
-        streams;
-
-    getVideoStream = function() {
-        return new Promise(function(resolve, error) {
-            navigator.webkitGetUserMedia({
-                audio: false,
-                video: {
-                    mandatory: {
-                        chromeMediaSource: 'desktop',
-                        chromeMediaSourceId: screenId,
-                        maxWidth: screen.width,
-                        maxHeight: screen.height
-                    }
-                }
-            }, resolve, error);
-        });
-    }
-
-    getAudioStream = function() {
-        return new Promise(function(resolve, error) {
-            navigator.webkitGetUserMedia({
-                audio: true
-            }, resolve, error);
-        });
-    }
-
-    return new Promise(function(resolve, reject) {
-        chrome.desktopCapture.chooseDesktopMedia(['screen', 'window'], resolve);
-    })
-    .then(function(id) {
-        screenId = id;
-
-        return Promise.all([
-            getVideoStream(),
-            getAudioStream()
-        ]);
-    })
-    .then(function(config) {
-        streams = {
-            video: config[0],
-            audio: config[1]
-        };
-    })
-    .then(function() {
-        return new Promise(function(resolve, error) {
-            videoEncoder = document.createElement("embed");
-
-            videoEncoder.setAttribute("width", 0);
-            videoEncoder.setAttribute("height", 0);
-            videoEncoder.setAttribute("src", "/nacl/manifest.nmf");
-            videoEncoder.setAttribute("ps_tty_prefix", "ps:");
-            videoEncoder.setAttribute("ps_stdout", "/dev/tty");
-            videoEncoder.setAttribute("ps_stderr", "/dev/tty");
-            videoEncoder.setAttribute("type", "application/x-nacl");
-
-            videoEncoder.addEventListener("load", resolve);
-
-            videoEncoder.addEventListener("message", function() {
-                console.log(arguments);
-            });
-
-            videoEncoder.addEventListener("crash", function() {
-                console.log(arguments);
-            });
-
-            // Request the offsetTop property to force a relayout. As of Apr 10, 2014
-            // this is needed if the module is being loaded on a Chrome App's
-            // background page (see crbug.com/350445).
-            document.body.appendChild(videoEncoder);
-            videoEncoder.offsetTop;
-        });
-    })
-    .then(function() {
-        videoEncoder.postMessage({
-            type: "start", 
-            data: {
-                filename: "/html5_persistent/output.webm",
-                chromeVersion: 46,
-                videoTrack: streams.video.getVideoTracks()[0],
-                audioTrack: streams.audio.getAudioTracks()[0]
-            }
-        });
-
-        return new Promise(function(resolve, error) {
-            streams.video.onended = resolve;
-        });
-    })
-    .then(function() {
-        videoEncoder.postMessage({
-            type: "stop", 
-            data: {}
-        });
-    })
-    .then(function() {
-        [streams.video, streams.audio].forEach(function(stream) {
-            stream.getTracks().forEach(function(track) {
-                track.stop();
-            });
-        });
-    })
-    .then(function() {
-        return FsHelper.readFileAsDataUrl("output.webm");
-    })
-    .then(callback);
-}
-
 function Recorder() {}
 Recorder.prototype = {
     start: function(callback) {
-        this.stream = null;
         this.recorder = null;
         this.buffer = [];
 
         this.getSource()
-            .then(this.getStream.bind(this))
+            .then(this.initStreams.bind(this))
+            .then(this.mergeStreams.bind(this))
             .then(this.record.bind(this))
             .then(this.cleanup.bind(this))
             .then(this.save.bind(this))
             .then(callback);
     },
 
-    getStream: function(sourceId) {
+    initStreams: function(sourceId) {
+        return Promise.all([
+            this.getVideoStream(sourceId),
+            this.getAudioStream()
+        ]);
+    },
+
+    getVideoStream: function(sourceId) {
         return new Promise(function(resolve, error) {
             navigator.webkitGetUserMedia({
-                audio: true,
+                audio: false,
                 video: {
                     mandatory: {
                         chromeMediaSource: 'desktop',
@@ -169,6 +34,23 @@ Recorder.prototype = {
                 }
             }, resolve, error);
         });
+    },
+
+    getAudioStream: function() {
+        return new Promise(function(resolve, error) {
+            navigator.webkitGetUserMedia({
+                audio: true,
+                video: false
+            }, resolve, error);
+        });
+    },
+
+    mergeStreams: function(streams) {
+        var video = streams[0],
+            audio = streams[1];
+
+        video.addTrack(audio.getTracks()[0])
+        return video;
     },
 
     getSource: function() {
@@ -186,7 +68,7 @@ Recorder.prototype = {
         return new Promise(function(resolve, error) {
             var recorder = new MediaRecorder(stream);
 
-            recorder.ondataavailable = function() {
+            recorder.ondataavailable = function(e) {
                 if(e.data)
                     me.buffer.push(e.data);
 
@@ -204,7 +86,7 @@ Recorder.prototype = {
             recorder = this.recorder;
 
         return new Promise(function(resolve, reject) {
-            stream.getTracks().forEach(function(track) {
+            recorder.stream.getTracks().forEach(function(track) {
                 track.stop();
             });
 
@@ -217,11 +99,17 @@ Recorder.prototype = {
     },
 
     save: function() {
-        var buffer = this.buffer;
+        var blob = new Blob(this.buffer, {
+            type: "video/webm"
+        });
 
-        return new Promise(function(resolve, reject) {
-            console.log(buffer);
-            resolve();
+        return new Promise(function(resolve, error) {
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                resolve(reader.result);
+            };
+
+            reader.readAsDataURL(blob);
         });
     }
 }
